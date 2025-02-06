73d8cfb57c7dd5d23eecd52a72cf56d264d10e7b
Aaron-Paatela <eh3375tx@go.minnstate.edu>
2025-02-06 15:55:18 -0600

    Add files via upload

---
+++ Homework 2/The Magic Cannon (page 89)/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace The_Magic_Cannon__page_89_
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("COMBINED BLAST!");
+                    
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Fire");
+                    
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Electric");
+                    
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Normal");
+                    
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+}
