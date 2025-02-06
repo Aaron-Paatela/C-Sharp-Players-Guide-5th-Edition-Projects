5d8e6397f1e4dbf449ad51a975f39c97c76b3e2d
Aaron-Paatela <eh3375tx@go.minnstate.edu>
2025-02-06 15:50:57 -0600

    Add files via upload

---
+++ Homework 2/The Prototype/Program.cs
@@ -0,0 +1,77 @@
+﻿// Written By Aaron Paatela
+// 2/5/2025
+
+
+using System.Reflection.Metadata.Ecma335;
+
+namespace The_Prototype
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Establishing our variables
+
+            bool validNumber = false;
+            int correctNumber = -1;
+            bool guessedCorrectly = false;
+            int numberGuess = -1;
+            string stringNumGuess = "";
+
+
+            // While loop asking user to enter a number and making sure a valid number was entered
+            while (validNumber == false)
+            {
+                Console.Write("User 1, enter a number between 0 and 100: ");
+                string userInput = Console.ReadLine();
+                correctNumber = Convert.ToInt32(userInput);
+                if (correctNumber < 0 || correctNumber > 100) // Checks whether the entered number is valid or not
+                {
+                    Console.WriteLine("number entered invalid, try again");
+                    continue;
+                }
+                else
+                {
+                    validNumber = true; // Could also just use a break statement here
+                    continue;
+                }
+
+                
+            }
+            Console.Clear();
+
+
+            Console.Write("User 2, guess the number. ");
+            stringNumGuess = Console.ReadLine();
+            numberGuess = Convert.ToInt32(stringNumGuess);
+
+            // While loop for number guesser
+            while (guessedCorrectly == false)
+            {
+                if (numberGuess < correctNumber)
+                {
+                    Console.WriteLine($"{numberGuess} is too low");
+                    Console.Write("What is your next guess?");
+                    stringNumGuess = Console.ReadLine();
+                    numberGuess = Convert.ToInt32(stringNumGuess);
+                    continue;
+                }
+                else if (numberGuess > correctNumber)
+                {
+                    Console.WriteLine($"{numberGuess} is too high");
+                    Console.Write("What is your next guess?");
+                    stringNumGuess = Console.ReadLine();
+                    numberGuess = Convert.ToInt32(stringNumGuess);
+                    continue;
+                }
+                else if (numberGuess == correctNumber)
+                {
+                    Console.WriteLine($"You guessed the number!");
+                    guessedCorrectly = true;
+                    continue;
+                }
+                
+            }
+        }
+    }
+}
---
+++ Homework 2/Watch Tower (page 78)/Program.cs
@@ -0,0 +1,72 @@
+﻿// Written By Aaron Paatela
+// 2/4/2025
+
+
+namespace Watch_Tower__page_78_
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            // User x-axis and y-axis input in relation to the tower location
+            // -x values == westward, +x values == eastward
+            // -y values == southward, +y values == northward
+            Console.Write("What is the enemy locations x axis value? ");
+            string xInput = Console.ReadLine();
+            int xAxisNum = Convert.ToInt32(xInput);
+
+            Console.Write("\nWhat is the enemy locations y axis value? ");
+            string yInput = Console.ReadLine();
+            int yAxisNum = Convert.ToInt32(yInput);
+
+
+            //Direction if and nested if statements to determine direction based on input
+            if (xAxisNum < 0)
+            {
+                if (yAxisNum > 0)
+                {
+                    Console.WriteLine("The enemy is to the NorthWest!");
+                }
+                else if (yAxisNum == 0)
+                {
+                    Console.WriteLine("The enemy is to the West!");
+                }
+                else if (yAxisNum < 0)
+                {
+                    Console.WriteLine("The enemy is to the SouthWest!");
+                }
+            }
+            if (xAxisNum == 0)
+            {
+                if (yAxisNum > 0)
+                {
+                    Console.WriteLine("The enemy is to the North!");
+                }
+                else if (yAxisNum == 0)
+                {
+                    Console.WriteLine("The enemy is right on top of us!");
+                }
+                else if (yAxisNum < 0)
+                {
+                    Console.WriteLine("The enemy is to the South!");
+                }
+            }
+            if (xAxisNum > 0)
+            {
+                if (yAxisNum > 0)
+                {
+                    Console.WriteLine("The enemy is to the NorthEast!");
+                }
+                else if (yAxisNum == 0)
+                {
+                    Console.WriteLine("The enemy is to the East!");
+                }
+                else if (yAxisNum < 0)
+                {
+                    Console.WriteLine("The enemy is to the SouthEast!");
+                }
+            }
+        }
+    }
+}
