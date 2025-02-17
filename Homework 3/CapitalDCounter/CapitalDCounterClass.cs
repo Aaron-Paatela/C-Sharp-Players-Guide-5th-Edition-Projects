7780227c9ff3a307b435b88707c9b5140d62d3a4
Aaron-Paatela <eh3375tx@go.minnstate.edu>
2025-02-16 20:44:30 -0600

    Add files via upload

---
+++ Homework 3/CapitalDCounter/CapitalDCounterClass.cs
@@ -0,0 +1,32 @@
+﻿// Written By Aaron Paatela
+// 02/11/2025
+// This is the separate class for the Homework 3 problem to count the number of capital D characters
+// in a string. 
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapitalDCounter
+{
+    public class CapitalDCounterClass
+    {
+        // methods
+        public int CountDs(string astring)
+        {
+            int DCount = 0;
+            foreach (char c in astring)
+            {
+                if (c == 'D')
+                {
+                    DCount++;
+                }    
+            }
+            return DCount;
+        }
+
+    }
+}
---
+++ Homework 3/CapitalDCounter/CapitalDCounterProgram.cs
@@ -0,0 +1,21 @@
+﻿// Written By Aaron Paatela
+// 02/11/2025
+// This is the Main Program for the Homework 3 problem to count the number of capital D characters
+// in a string.  
+
+
+
+namespace CapitalDCounter
+{
+    public class CapitalDCounterProgram
+    {
+        public static void Main(string[] args)
+        {
+            CapitalDCounterClass aDCounter = new CapitalDCounterClass();
+
+            string myString = "There are DDDDD 6 D's in this string.";
+
+            Console.WriteLine($"The number of capital Ds in '{myString}' is {aDCounter.CountDs(myString)}.");
+        }
+    }
+}
