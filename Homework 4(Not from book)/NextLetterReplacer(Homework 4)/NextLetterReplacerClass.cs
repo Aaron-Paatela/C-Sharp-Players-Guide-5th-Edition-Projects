e530d60325d7303a6c446b1885d3464618ee9ed0
Aaron-Paatela <eh3375tx@go.minnstate.edu>
2025-02-17 17:35:41 -0600

    Add files via upload

---
+++ Homework 4(Not from book)/NextLetterReplacer(Homework 4)/NextLetterReplacerClass.cs
@@ -0,0 +1,36 @@
+﻿// Written By Aaron Paatela
+// 2/17/2025
+// This is the separate class for the Homework 4 exercise to
+// write a function that changes every letter in a string to the next letter
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextLetterReplacer
+{
+    public class NextLetterReplacerClass
+    {
+
+        public static string Move(string aString)
+        {
+            string replacedString = "";
+            
+            for (int i = 0;  i < aString.Length; i++)
+            {
+                int encodingNum = aString[i]; // takes the integer value for the character at the index location in the string
+
+                replacedString += Char.ConvertFromUtf32(encodingNum + 1); //appends the string we are returning with the letter that
+                                                                          // corresponds to the number we got before +1
+                                                                          // (the next letter in the table).
+                                                                          // This is also why it wouldn't work for 'z'.
+                                                                          // In the case of 'z', we would get back '{'.
+                                                                          // In the case of 'Z', we would get back '['.
+            }
+            return replacedString;
+        }
+    }
+}
---
+++ Homework 4(Not from book)/NextLetterReplacer(Homework 4)/NextLetterReplacerProgram.cs
@@ -0,0 +1,20 @@
+﻿// Written By Aaron Paatela
+// 2/17/2025
+// This is the Main Program for the Homework 4 exercise to
+// write a function that changes every letter in a string to the next letter
+
+namespace NextLetterReplacer
+{
+    public class NextLetterReplacerProgram
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(NextLetterReplacerClass.Move("hello"));
+
+            Console.WriteLine(NextLetterReplacerClass.Move("bye"));
+
+            Console.WriteLine(NextLetterReplacerClass.Move("welcome"));
+
+        }
+    }
+}
