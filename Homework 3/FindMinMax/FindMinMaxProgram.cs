8e3a03312bafd5e073e62fd46e8f65cd5bd3dfdd
Aaron-Paatela <eh3375tx@go.minnstate.edu>
2025-02-16 20:43:20 -0600

    Add files via upload

---
+++ Homework 3/FindMinMax/FindMinMaxProgram.cs
@@ -0,0 +1,21 @@
+﻿// Written By Aaron Paatela
+// 2/11/2025
+// This is the Main Program for the Homework 3 problem to find the min and max of an array
+
+
+
+namespace FindMinMax
+{
+    public class FindMinMaxProgram
+    {
+        public static void Main(string[] args)
+        {
+            FindMinMaxSolution solver = new FindMinMaxSolution();
+            int[] output = solver.FindMinMax([1, 4532, 7, 56, 24, -1, 56, 2, 52, 543, 23]);
+
+            Console.WriteLine($"Min: {output[0]}.");
+            Console.WriteLine($"Max: {output[1]}.");
+            
+        }
+    }
+}
---
+++ Homework 3/FindMinMax/FindMinMaxSolution.cs
@@ -0,0 +1,37 @@
+﻿// Written By Aaron Paatela
+// 2/11/2025
+// This is the separate class for the Homework 3 problem to find the min and max of an array
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMinMax
+{
+    public class FindMinMaxSolution
+    {
+        // methods
+        public int[] FindMinMax(int[] arr)
+        {
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            foreach (int i in arr)
+            {
+                if( i < min)
+                {
+                    min = i;
+                }
+                if ( i > max)
+                { 
+                    max = i;
+                } 
+                    
+            }
+            return [min, max];
+        }
+    }
+}
