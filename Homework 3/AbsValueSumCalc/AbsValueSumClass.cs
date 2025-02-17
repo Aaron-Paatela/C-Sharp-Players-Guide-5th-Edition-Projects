263adc6659460366ae4ad3f419b6c4a032ea8109
Aaron-Paatela <eh3375tx@go.minnstate.edu>
2025-02-16 20:43:47 -0600

    Add files via upload

---
+++ Homework 3/AbsValueSumCalc/AbsValueSumClass.cs
@@ -0,0 +1,30 @@
+﻿// Written By Aaron Paatela
+// 2/11/2025
+// This is the separate class for the Homework 3 problem to find the absolute value sum of
+// an array of integers
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbsValueSumCalc
+{
+    public class AbsValueSumClass
+    {
+        // methods
+        public int GetAbsValue(int[] ints)
+        {
+            int sum = 0;
+            foreach (int i in ints)
+            {
+                sum += Math.Abs(i);
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ Homework 3/AbsValueSumCalc/AbsValueSumProgram.cs
@@ -0,0 +1,22 @@
+﻿// Written By Aaron Paatela
+// 2/11/2025
+// This is the Main Program for the Homework 3 problem to find the absolute value sum of
+// an array of integers
+
+
+using System.Security.Cryptography.X509Certificates;
+
+namespace AbsValueSumCalc
+{
+    public class AbsValueSumProgram
+    {
+        public static void Main(string[] args)
+        {
+            AbsValueSumClass sumInstance = new AbsValueSumClass();
+
+            int solution = sumInstance.GetAbsValue([-1, -2, -3, -4, 5, 6, 7]);
+
+            Console.WriteLine(solution);
+        }
+    }
+}
