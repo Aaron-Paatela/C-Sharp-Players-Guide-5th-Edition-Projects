cbd86b9521da8eb89bbbd0c95c4da69410ef3a91
Aaron-Paatela <eh3375tx@go.minnstate.edu>
2025-02-17 17:37:27 -0600

    Add files via upload

---
+++ Homework 4(Not from book)/NextLetterReplacer(Homework 4)/IsInOrder(Homework 4)/IsInOrderProgram.cs
@@ -0,0 +1,31 @@
+﻿// Written By Aaron Paatela
+// 2/17/2025
+// This is the Main Program for the Homework 4 exercise to
+// create a function that takes a string and returns true or false, depending on whether the characters
+// are in order or not.  
+
+// As a side note, since this program compares the ASCII values for the given letter, it will determine that any capital letter
+// is 'valued' at less than any lower case letter.  Thus, Y < Z < a.  numbers will also be 'valued' less han either.
+// So this particular function will work best if the individual string only contains ints 0-9, letters A-Z, or letters a - z.
+
+
+
+namespace IsInOrder_Homework_4_
+{
+    public  class IsInOrderProgram
+    {
+        public static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine(IsInOrderClass.CheckOrder("abc"));
+            Console.WriteLine(IsInOrderClass.CheckOrder("cba"));
+            Console.WriteLine(IsInOrderClass.CheckOrder("123"));
+            Console.WriteLine(IsInOrderClass.CheckOrder("321"));
+            Console.WriteLine(IsInOrderClass.CheckOrder("edabit"));
+            Console.WriteLine(IsInOrderClass.CheckOrder("xyzz"));
+
+
+        }
+    }
+}
---
+++ Homework 4(Not from book)/NextLetterReplacer(Homework 4)/VowelReturner (Homework 4)/VowelReturnerProgram.cs
@@ -0,0 +1,21 @@
+﻿// Written By Aaron Paatela
+// 2/17/2025
+// This is the separate class for the Homework 4 exercise to 
+// write a function that returns the first n vowels of a string.
+
+
+namespace VowelReturner__Homework_4_
+{
+    public class VowelReturnerProgram
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(VowelReturnerClass.FirstNVowels("sharpening skills", 3));
+
+            Console.WriteLine(VowelReturnerClass.FirstNVowels("major league", 5));
+
+            Console.WriteLine(VowelReturnerClass.FirstNVowels("hostess", 5));
+
+        }
+    }
+}
