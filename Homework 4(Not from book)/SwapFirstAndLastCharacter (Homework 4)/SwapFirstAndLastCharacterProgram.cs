503032c80c35fe3a9d195f0b06bde25a23dcbb34
Aaron-Paatela <eh3375tx@go.minnstate.edu>
2025-02-17 17:40:12 -0600

    Add files via upload

---
+++ Homework 4(Not from book)/SwapFirstAndLastCharacter (Homework 4)/SwapFirstAndLastCharacterProgram.cs
@@ -0,0 +1,31 @@
+﻿// Written By Aaron Paatela
+// 2/17/2025
+// This is the separate class for the Homework 4 exercise
+// to swap the first and last character in a string
+// unless the length of the string is less than two,
+
+
+
+namespace SwapFirstAndLastCharacter__Homework_4_
+{
+    internal class SwapFirstAndLastCharacterProgram
+    {
+        public static void Main(string[] args)
+        {
+            string exampleString = "Cat, dog, and mouse.";
+
+            string flippedString = SwapFirstAndLastCharacterClass.SwapCharacters(exampleString);
+
+            string exampleTwoPair = "ada";
+            string flippedPair = SwapFirstAndLastCharacterClass.SwapCharacters(exampleTwoPair);
+
+            string OneLetterExample = SwapFirstAndLastCharacterClass.SwapCharacters("z");
+
+            Console.WriteLine(flippedString);
+
+            Console.WriteLine(flippedPair);
+
+            Console.WriteLine(OneLetterExample);
+        }
+    }
+}
