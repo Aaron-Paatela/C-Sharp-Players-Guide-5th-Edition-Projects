be415ff65df789fa53cdc6cc60cfc784772097c3
Aaron-Paatela <eh3375tx@go.minnstate.edu>
2025-02-16 20:33:30 -0600

    Add files via upload

---
+++ Homework 3/Hunting The Manticore (page 124)/HuntingTheManticoreClass.cs
@@ -0,0 +1,106 @@
+﻿// Written By Aaron Paatela
+// 2/12/2025
+// This is the separate class for the Hunting The Manticore exercise
+// in the C# Players Guide 5th Edition on page 124
+
+
+using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunting_The_Manticore__page_124_
+{
+    public class HuntingTheManticoreClass
+    {
+        // variables
+
+        private int cityHealth = 0;
+        private int manticoreHealth = 0;
+        private int currentRound = 0;
+        private int manticoreLocation = int.MaxValue;
+
+        // gets and sets
+
+        public int CityHealth
+        {
+            get { return this.cityHealth; }
+            set { this.cityHealth = value; }
+        }
+        public int ManticoreHealth
+        {
+            get { return this.manticoreHealth; }
+            set { this.manticoreHealth = value; }
+        }
+        public int CurrentRound
+        {
+            get { return this.currentRound; }
+            set { this.currentRound = value; }
+        }
+        public int ManticoreLocation
+        {
+            get { return this.manticoreLocation; }
+            set { this.manticoreLocation = value; }
+        }
+
+
+
+
+        // Methods
+
+        public string CannonShot(int anInt)
+        {
+            string message = string.Empty;
+            if (anInt == ManticoreLocation)
+            {
+                if (CurrentRound % 5 == 0 && CurrentRound % 3 == 0)
+                {
+                    ManticoreHealth -= 10;
+                }
+
+                else if (CurrentRound % 3 == 0 || CurrentRound % 5 == 0)
+                {
+                    ManticoreHealth -= 3; 
+                }  
+                else
+                {
+                    ManticoreHealth -= 1;
+                }
+                message += "That round was a DIRECT HIT!";
+                
+            }
+            else if ( anInt < ManticoreLocation )
+            {
+                message += "That round FELL SHORT of the target.";
+            }
+            else
+            {
+                message += "That round OVERSHOT the target.";
+            }
+            return message;
+
+        }
+
+
+
+
+
+
+        // constructors
+
+        public HuntingTheManticoreClass():this(15, 10, 1, int.MaxValue)
+        {
+
+        }
+
+        public HuntingTheManticoreClass(int _CityHealth, int _ManticoreHealth, int _CurrentRound, int _ManticoreLocation)
+        {
+            this.CityHealth = _CityHealth;
+            this.ManticoreHealth = _ManticoreHealth;
+            this.CurrentRound = _CurrentRound;
+            this.ManticoreLocation = _ManticoreLocation;
+        }
+    }
+}
---
+++ Homework 3/Hunting The Manticore (page 124)/HuntingTheManticoreProgram.cs
@@ -0,0 +1,89 @@
+﻿// Written By Aaron Paatela
+// 2/12/2025
+// This is the Main Program for the Hunting The Manticore exercise
+// in C# Players Guide 5th Edition on page 124
+
+
+using System.Runtime.CompilerServices;
+
+namespace Hunting_The_Manticore__page_124_
+{
+    public class HuntingTheManticoreProgram
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.Write("Player 1, how far away from the city do you want to station the Manticore? ");
+            int testNum = Convert.ToInt32(Console.ReadLine());
+            int manticoreLocation = int.MinValue;
+            // Makes sure the manticore location is a valid number (0 to 100 inclusive)
+            bool validNum = false;
+            while (validNum == false)
+            {
+                if (testNum < 0 || testNum > 100)
+                {
+                    Console.WriteLine("Invalid Number. Please try again (0 through 100): ");
+                    testNum = Convert.ToInt32(Console.ReadLine());
+                    continue;
+                }
+                else
+                {
+                    manticoreLocation = testNum;
+                    validNum = true;
+                    continue;
+                }
+            }
+
+            Console.Clear();
+
+            HuntingTheManticoreClass aManticoreHunt = new HuntingTheManticoreClass(15, 10, 1, manticoreLocation);
+
+            // initializing the game
+            bool gameOver = false;
+            Console.WriteLine("Player 2: It is your turn");
+            while (gameOver == false)
+            {
+                Console.WriteLine("-------------------------------------------------------------");
+                Console.WriteLine($"STATUS: Round: {aManticoreHunt.CurrentRound}   City: {aManticoreHunt.CityHealth}/15" +
+                    $"   Manticore: {aManticoreHunt.ManticoreHealth}/10");
+                if (aManticoreHunt.CurrentRound % 5 == 0 && aManticoreHunt.CurrentRound % 3 == 0)
+                {
+                    Console.WriteLine("The Cannon is expected to deal 10 damage this turn.");
+                }
+                else if (aManticoreHunt.CurrentRound % 5 == 0 || aManticoreHunt.CurrentRound % 3 == 0)
+                {
+                    Console.WriteLine("The Cannon is expected to deal 3 damage this turn.");
+                }
+                else
+                {
+                    Console.WriteLine("The Cannon is expected to deal 1 damage this turn.");
+                }
+                Console.Write("Enter desired cannon range: ");
+                int desiredRange = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(aManticoreHunt.CannonShot(desiredRange));
+                aManticoreHunt.CurrentRound++;
+                aManticoreHunt.CityHealth--;
+                if (aManticoreHunt.ManticoreHealth <= 0)
+                {
+                    Console.WriteLine("The Manticore has been destroyed! The city of Consolas has been saved!");
+                    gameOver = true;
+                    continue;
+                }    
+                else if (aManticoreHunt.CityHealth <= 0)
+                {
+                    Console.WriteLine("The Manticore has destroyed the city! Better luck next time!");
+                    gameOver = true;
+                    continue;
+                }
+                else
+                {
+                    continue;
+                }
+
+
+
+
+            }
+        }
+    }
+}
