399c8c07577c6c40b903bd1882f0d1187cf8f881
Aaron-Paatela <eh3375tx@go.minnstate.edu>
2025-02-16 20:38:20 -0600

    Add files via upload

---
+++ Homework 3/SmallerNumberComparer/SmallerNumberClass.cs
@@ -0,0 +1,58 @@
+﻿// Written By Aaron Paatela
+// 2/11/2025
+// This is the separate class for the Homework 3 problem to return the smaller number between 2 strings
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallerNumberComparer
+{
+    public class SmallerNumberClass
+    {
+
+        // methods
+        
+        public string GetSmallerNum(string num1, string num2)
+            // This method probably warrants explanations for each statement
+        {
+            // if one string is shorter than the other, the shorter string is always a lower number
+            // 100 will always be more than any number 1 - 99. 
+            if (num1.Length < num2.Length)
+            {
+                return num1;
+            }
+            else if (num2.Length < num1.Length)
+            {
+                return num2;
+            }
+            else
+            {
+                // If the numbers are equal in length, you compare each number one by one
+                // This works without conversion because in ASCII, each number 0 - 9
+                // is valued lower than the one above it.  In other words,
+                // the ASCII value for 0 < ASCII value for 1 < ACSII value for 2
+                // I had to look this up to figure out how I could compare the numbers
+                // without converting to integers.
+                for (int i = 0; i < num1.Length; i++)
+                {
+                    if (num1[i] < num2[i])
+                    {
+                        return num1;
+                    }
+                    else if (num1[i] >  num2[i])
+                    {
+                        return num2;
+                    }
+                }
+                // If the length is the same and each individual ASCII number is never < or >
+                // when comparing to the other, you have the same number. So return whichever one you want.
+                return num1;
+            }
+        }
+    }
+}
---
+++ Homework 3/SmallerNumberComparer/SmallerNumberProgram.cs
@@ -0,0 +1,22 @@
+﻿// Written By Aaron Paatela
+// 2/11/2025
+// This is the Main Program for the Homework 3 problem to return the smaller number between 2 strings
+
+
+
+namespace SmallerNumberComparer
+{
+    public class SmallerNumberProgram
+    {
+        public static void Main(string[] args)
+        {
+
+            SmallerNumberClass classInstance = new SmallerNumberClass();
+
+            string result = classInstance.GetSmallerNum("120", "2");
+
+            Console.WriteLine(result);
+            
+        }
+    }
+}
